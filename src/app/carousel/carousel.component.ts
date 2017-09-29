31fd2d319921f303e739509c4595bd9f7acb38d4
alexfuser <alexfuser@mozilla-mexico.org>
2017-09-29 13:03:47 -0500

    New carousel

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -12,12 +12,13 @@ export class CarouselComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    $(document).ready(function(ev){
-      $('#custom_carousel').on('slide.bs.carousel', function (evt) {
-        $('#custom_carousel .controls li.active').removeClass('active');
-        $('#custom_carousel .controls li:eq('+$(evt.relatedTarget).index()+')').addClass('active');
-      })
-  });
+    $(document).ready(function() {
+      
+      $('#myCarouselArticle').carousel({
+                      interval: 1000
+              });
+        
+      });
   }
 
 }
